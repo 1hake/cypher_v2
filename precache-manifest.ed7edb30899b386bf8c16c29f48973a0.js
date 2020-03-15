8b18dd759b1c6d31d590190b756b881ec37e3c26
Colin Champdavoine <colinchampdavoine@Colins-MacBook-Pro.local>
2020-03-15 15:49:36 +0100

    Updates

---
+++ precache-manifest.ed7edb30899b386bf8c16c29f48973a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4e5ea79dfbeaf32f8e4395ca19fddf",
+    "url": "/cypher-project-v2/index.html"
+  },
+  {
+    "revision": "010d70cecf52b4ba5fd0",
+    "url": "/cypher-project-v2/static/css/main.365d095a.chunk.css"
+  },
+  {
+    "revision": "9f0f053532cf47f083c9",
+    "url": "/cypher-project-v2/static/js/2.462f197b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cypher-project-v2/static/js/2.462f197b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "010d70cecf52b4ba5fd0",
+    "url": "/cypher-project-v2/static/js/main.c9c525dc.chunk.js"
+  },
+  {
+    "revision": "0f3912f488281256bac7",
+    "url": "/cypher-project-v2/static/js/runtime-main.7e678fb5.js"
+  }
+]);
